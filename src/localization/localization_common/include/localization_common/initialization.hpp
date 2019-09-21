0053b4e61a8d92e9bd8ec108a4d2a6df53a1f19a
yunus.caliskan <yunus.ec@gmail.com>
2019-09-21 22:43:31 +0200

    Defined the base interface for the relative localizer.

---
+++ src/localization/localization_common/include/localization_common/initialization.hpp
@@ -0,0 +1,27 @@
+#include <geometry_msgs/msg/transform.hpp>
+#include <tf2/buffer_core.h>
+// probably include the motion model
+
+namespace autoware
+{
+namespace localization
+{
+namespace localization_common
+{
+
+template <typename Derived>
+class PoseInitializationBase{
+    using PoseT = geometry_msgs::msg::Transform;
+
+    PoseT guess_initial(const tf2::BufferCore &);
+};
+
+class VehicleOdometryPoseInitialization : public PoseInitializationBase<VehicleOdometryPoseInitialization>{
+    using PoseT = geometry_msgs::msg::Transform;
+
+    PoseT guess_initial(const tf2::BufferCore &);
+};
+
+}          // namespace autoware
+}      // namespace localization
+}  // namespace localization_common
