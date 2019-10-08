526f071b68f04ec9c32d7b02f2abe3d08165fd18
Christopher Ho <christopher.ho@apex.ai>
2019-10-08 11:36:33 -0700

    [36] Port pure_pursuit_nodes to ControllerBaseNode

--- src/motion/control/pure_pursuit_nodes/include/pure_pursuit_nodes/pure_pursuit_node.hpp
+++ src/motion/control/pure_pursuit_nodes/include/pure_pursuit_nodes/pure_pursuit_node.hpp
@@ -6,17 +6,11 @@
 #ifndef PURE_PURSUIT_NODES__PURE_PURSUIT_NODE_HPP_
 #define PURE_PURSUIT_NODES__PURE_PURSUIT_NODE_HPP_
 
-#include <rclcpp/rclcpp.hpp>
-#include <autoware_auto_msgs/msg/trajectory.hpp>
-#include <autoware_auto_msgs/msg/vehicle_kinematic_state.hpp>
-#include <autoware_auto_msgs/msg/control_diagnostic.hpp>
-#include <autoware_auto_msgs/msg/vehicle_control_command.hpp>
-#include <geometry_msgs/msg/transform_stamped.hpp>
-#include <tf2_msgs/msg/tf_message.hpp>
-#include <tf2/buffer_core.h>
+#include <controller_common_nodes/controller_base_node.hpp>
 #include <pure_pursuit/pure_pursuit.hpp>
+
 #include <string>
-#include <vector>
+
 #include "pure_pursuit_nodes/visibility_control.hpp"
 
 namespace autoware
@@ -30,7 +24,8 @@ namespace pure_pursuit_nodes
 {
 /// \brief Boilerplate node that subscribes to the current pose and
 /// publishes a vehicle control command
-class PURE_PURSUIT_NODES_PUBLIC PurePursuitNode : public rclcpp::Node
+class PURE_PURSUIT_NODES_PUBLIC PurePursuitNode
+  : public ::motion::control::controller_common_nodes::ControllerBaseNode
 {
 public:
   /// \brief Parameter constructor
@@ -45,6 +40,7 @@ public:
   /// \param[in] pose_topic Name of input pose topic
   /// \param[in] trajectory_topic Name of input trajectory topic
   /// \param[in] command_topic Name of output control command topic
+  /// \param[in] tf_topic Name of input tf topic
   /// \param[in] diagnosis_topic Name of output diagnosis topic
   /// \param[in] cfg Configuration object for PurePursuit
   /// \param[in] node_namespace Namespace of this node
@@ -54,39 +50,9 @@ public:
     const std::string & trajectory_topic,
     const std::string & command_topic,
     const std::string & diagnosis_topic,
+    const std::string & tf_topic,
     const pure_pursuit::Config & cfg,
     const std::string & node_namespace = "");
-
-private:
-  using Trajectory = autoware_auto_msgs::msg::Trajectory;
-  using TrajectoryPointStamped = autoware_auto_msgs::msg::VehicleKinematicState;
-  using ControllerDiagnostic = autoware_auto_msgs::msg::ControlDiagnostic;
-  using VehicleControlCommand = autoware_auto_msgs::msg::VehicleControlCommand;
-  using Transform = geometry_msgs::msg::Transform;
-  using Transforms = tf2_msgs::msg::TFMessage;
-
-  PURE_PURSUIT_NODES_LOCAL void init(
-    const std::string & pose_topic,
-    const std::string & trajectory_topic,
-    const std::string & diagnostic_topic,
-    const std::string & command_topic);
-  PURE_PURSUIT_NODES_LOCAL void on_trajectory(const Trajectory::SharedPtr msg);
-  PURE_PURSUIT_NODES_LOCAL void on_pose(const TrajectoryPointStamped::SharedPtr msg);
-  PURE_PURSUIT_NODES_LOCAL void on_tf(const Transforms::SharedPtr msg);
-
-  /// \brief Core run loop
-  void function(TrajectoryPointStamped pose);
-
-  pure_pursuit::PurePursuit m_controller;
-  tf2::BufferCore m_tf{};
-
-  rclcpp::Subscription<TrajectoryPointStamped>::SharedPtr m_pose_sub_ptr{};
-  rclcpp::Subscription<Trajectory>::SharedPtr m_traj_sub_ptr{};
-  rclcpp::Subscription<Transforms>::SharedPtr m_tf_sub_ptr{};
-  typename rclcpp::Publisher<VehicleControlCommand>::SharedPtr m_cmd_pub_ptr{};
-  typename rclcpp::Publisher<ControllerDiagnostic>::SharedPtr m_diag_pub_ptr{};
-
-  bool m_traj_initialized{false};
 };
 }  // namespace pure_pursuit_nodes
 }  // namespace control
--- src/motion/control/pure_pursuit_nodes/src/pure_pursuit_node.cpp
+++ src/motion/control/pure_pursuit_nodes/src/pure_pursuit_node.cpp
@@ -2,13 +2,10 @@
 // All rights reserved.
 
 #include <motion_common/motion_common.hpp>
-#include <time_utils/time_utils.hpp>
 
-#include <cmath>
-#include <chrono>
-#include <utility>
+#include <memory>
 #include <string>
-#include <vector>
+
 #include "pure_pursuit_nodes/pure_pursuit_node.hpp"
 
 namespace autoware
@@ -25,28 +22,24 @@ using float32_t = float;
 PurePursuitNode::PurePursuitNode(
   const std::string & node_name,
   const std::string & node_namespace)
-: Node{node_name, node_namespace},
-  m_controller(pure_pursuit::Config{
-          static_cast<float32_t>(declare_parameter(
-            "controller.minimum_lookahead_distance").get<double>()),
-          static_cast<float32_t>(declare_parameter(
-            "controller.maximum_lookahead_distance").get<double>()),
-          static_cast<float32_t>(
-            declare_parameter("controller.speed_to_lookahead_ratio").get<double>()),
-          declare_parameter("controller.is_interpolate_lookahead_point").get<bool>(),
-          declare_parameter("controller.is_delay_compensation").get<bool>(),
-          static_cast<float32_t>(declare_parameter(
-            "controller.emergency_stop_distance").get<double>()),
-          static_cast<float32_t>(declare_parameter(
-            "controller.speed_thres_traveling_direction").get<double>()),
-          static_cast<float32_t>(declare_parameter(
-            "controller.dist_front_rear_wheels").get<double>())})
+: ControllerBaseNode{node_name, node_namespace}
 {
-  init(
-    declare_parameter("pose_topic").get<std::string>(),
-    declare_parameter("trajectory_topic").get<std::string>(),
-    declare_parameter("diagnosis_topic").get<std::string>(),
-    declare_parameter("command_topic").get<std::string>());
+  pure_pursuit::Config cfg{
+    static_cast<float32_t>(declare_parameter(
+      "controller.minimum_lookahead_distance").get<double>()),
+    static_cast<float32_t>(declare_parameter(
+      "controller.maximum_lookahead_distance").get<double>()),
+    static_cast<float32_t>(
+      declare_parameter("controller.speed_to_lookahead_ratio").get<double>()),
+    declare_parameter("controller.is_interpolate_lookahead_point").get<bool>(),
+    declare_parameter("controller.is_delay_compensation").get<bool>(),
+    static_cast<float32_t>(declare_parameter(
+      "controller.emergency_stop_distance").get<double>()),
+    static_cast<float32_t>(declare_parameter(
+      "controller.speed_thres_traveling_direction").get<double>()),
+    static_cast<float32_t>(declare_parameter(
+      "controller.dist_front_rear_wheels").get<double>())};
+  set_controller(std::make_unique<pure_pursuit::PurePursuit>(cfg));
 }
 ////////////////////////////////////////////////////////////////////////////////
 PurePursuitNode::PurePursuitNode(
@@ -55,81 +48,13 @@ PurePursuitNode::PurePursuitNode(
   const std::string & trajectory_topic,
   const std::string & command_topic,
   const std::string & diagnosis_topic,
+  const std::string & tf_topic,
   const pure_pursuit::Config & cfg,
   const std::string & node_namespace)
-: Node(node_name, node_namespace),
-  m_controller(cfg)
+: ControllerBaseNode{node_name, node_namespace, command_topic, pose_topic,
+    tf_topic, trajectory_topic, diagnosis_topic}
 {
-  init(pose_topic, trajectory_topic, diagnosis_topic, command_topic);
-}
-////////////////////////////////////////////////////////////////////////////////
-void PurePursuitNode::init(
-  const std::string & pose_topic,
-  const std::string & trajectory_topic,
-  const std::string & diagnostic_topic,
-  const std::string & command_topic)
-{
-  // Subscribers
-  m_pose_sub_ptr = create_subscription<TrajectoryPointStamped>(
-    pose_topic,
-    rclcpp::QoS{10},
-    [this](const TrajectoryPointStamped::SharedPtr msg) -> void {on_pose(msg);});
-  m_traj_sub_ptr = create_subscription<Trajectory>(
-    trajectory_topic,
-    rclcpp::QoS{10},
-    [this](const Trajectory::SharedPtr msg) -> void {on_trajectory(msg);});
-  m_tf_sub_ptr = create_subscription<Transforms>(
-    "tf",
-    rclcpp::QoS{10},
-    [this](const Transforms::SharedPtr msg) -> void {on_tf(msg);});
-  // Publishers
-  m_cmd_pub_ptr = create_publisher<VehicleControlCommand>(command_topic, rclcpp::QoS{10});
-  m_diag_pub_ptr = create_publisher<ControllerDiagnostic>(diagnostic_topic, rclcpp::QoS{10});
-}
-////////////////////////////////////////////////////////////////////////////////
-void PurePursuitNode::on_trajectory(const Trajectory::SharedPtr msg)
-{
-  m_controller.set_trajectory(*msg);
-  m_traj_initialized = true;
-}
-////////////////////////////////////////////////////////////////////////////////
-void PurePursuitNode::on_pose(const TrajectoryPointStamped::SharedPtr msg)
-{
-  function(*msg);
-}
-////////////////////////////////////////////////////////////////////////////////
-void PurePursuitNode::on_tf(const Transforms::SharedPtr msg)
-{
-  for (const auto & tf : msg->transforms) {
-    m_tf.setTransform(tf, "pure_pursuit_node");
-  }
-}
-////////////////////////////////////////////////////////////////////////////////
-void PurePursuitNode::function(TrajectoryPointStamped pose)
-{
-  // Take a trajectory
-  const auto now = std::chrono::system_clock::now();
-  const auto t_trajectory = time_utils::from_message(m_controller.get_trajectory().header.stamp);
-  const auto can_transform = m_tf.canTransform(
-    pose.header.frame_id.c_str(),
-    m_controller.get_trajectory().header.frame_id.c_str(),
-    t_trajectory);
-  if (can_transform) {
-    const auto pose_to_traj = m_tf.lookupTransform(
-      pose.header.frame_id.c_str(),
-      m_controller.get_trajectory().header.frame_id.c_str(),
-      t_trajectory);
-    auto pose_tf = pose;
-    ::motion::motion_common::doTransform(pose, pose_tf, pose_to_traj);
-    const auto & cmd = m_controller.compute_command(pose_tf);
-    m_cmd_pub_ptr->publish(cmd);
-    const auto & diagnostic = m_controller.get_diagnostic();
-    m_diag_pub_ptr->publish(diagnostic);
-  } else {
-    // Since the trajectory is taken successfully, TF graph or the trajectory's timestamp
-    // has problems. Fatal error.
-    RCLCPP_WARN(get_logger(), "PurePursuitNode: could not subscribe the tf topic");
-  }
+  set_controller(std::make_unique<pure_pursuit::PurePursuit>(cfg));
 }
 }  // namespace pure_pursuit_nodes
 }  // namespace control
