b9d906d92f4598697566524f07db0d18542756d5
Sandro Merkli <merkli@embotech.com>
2020-01-13 12:11:20 +0100

    Added a copy of Chris' planner nodes and renamed to "mock"
    This will serve as a basis for the mock planner - I'll strip out all the
    functionality that is not needed and substitute the mock functionality.

---
+++ src/motion/planning/mock_planner_node/src/mock_planner_node/mock_planner_main.cpp
@@ -0,0 +1,32 @@
+// Copyright 2019 Christopher Ho
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mock_planner_node/mock_planner_node.hpp"
+
+#include <memory>
+
+int32_t main(int32_t argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  using motion::planning::mock_planner_node::MockPlannerNode;
+  const auto nd = std::make_shared<MockPlannerNode>("mock_planner", "");
+
+  rclcpp::spin(nd);
+
+  if (!rclcpp::shutdown()) {
+    throw std::runtime_error{"rclcpp shutdown failed"};
+  }
+  return 0;
+}
