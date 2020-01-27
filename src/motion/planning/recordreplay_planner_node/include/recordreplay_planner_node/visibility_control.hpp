2fe74672e50400b959649235622608e301052f20
Sandro Merkli <merkli@embotech.com>
2020-01-27 10:24:24 +0100

    Final set of copyright changes

--- src/motion/planning/recordreplay_planner_node/include/recordreplay_planner_node/visibility_control.hpp
+++ src/motion/planning/recordreplay_planner_node/include/recordreplay_planner_node/visibility_control.hpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Sandro Merkli, inspired by Christopher Ho's mpc code
+// Copyright 2020 Embotech AG, Zurich, Switzerland, inspired by Christopher Ho's mpc code
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
