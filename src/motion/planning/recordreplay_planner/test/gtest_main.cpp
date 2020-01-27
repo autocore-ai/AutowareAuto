6e5d641e0728dea5ae57739dca0e237d8138cd19
Sandro Merkli <merkli@embotech.com>
2020-01-27 10:22:03 +0100

    Forgot some copyright changes

--- src/motion/planning/recordreplay_planner/test/gtest_main.cpp
+++ src/motion/planning/recordreplay_planner/test/gtest_main.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019 Christopher Ho
+// Copyright 2020 Embotech AG, Zurich, Switzerland, inspired by Christopher Ho's mpc code
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
