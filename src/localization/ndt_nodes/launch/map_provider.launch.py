85f69d0f6a8dea7382aea620846b7ac179eff02a
weihung.ko <weihung.ko@apex.ai>
2020-05-26 10:06:27 -0700

    [#216] Fix flake8 issues in ndt_nodes package

--- src/localization/ndt_nodes/launch/map_provider.launch.py
+++ src/localization/ndt_nodes/launch/map_provider.launch.py
@@ -15,6 +15,7 @@
 # Co-developed by Tier IV, Inc. and Apex.AI, Inc.
 
 import os
+
 from ament_index_python import get_package_share_directory
 import launch
 import launch.actions
@@ -26,30 +27,30 @@ def generate_launch_description():
 
     # map_provider parameter file definition
     ndt_map_provider_file_path = os.path.join(
-        get_package_share_directory("ndt_nodes"),
-        "param",
-        "map_publisher.param.yaml")
+        get_package_share_directory('ndt_nodes'),
+        'param',
+        'map_publisher.param.yaml')
     map_provider_param_file = launch.substitutions.LaunchConfiguration(
-        "params", default=[ndt_map_provider_file_path])
+        'params', default=[ndt_map_provider_file_path])
 
     # map_provide node execution definition
     map_provider_node_runner = launch_ros.actions.Node(
-        package="ndt_nodes",
-        node_executable="ndt_map_publisher_exe",
+        package='ndt_nodes',
+        node_executable='ndt_map_publisher_exe',
         parameters=[map_provider_param_file])
 
     # map downsampler paramter file definition
     ndt_map_downsampler_file_path = os.path.join(
-        get_package_share_directory("ndt_nodes"),
-        "param",
-        "pcl_map_voxel_grid_downsample.param.yaml")
+        get_package_share_directory('ndt_nodes'),
+        'param',
+        'pcl_map_voxel_grid_downsample.param.yaml')
     map_downsampler_param_file = launch.substitutions.LaunchConfiguration(
-        "params", default=[ndt_map_downsampler_file_path])
+        'params', default=[ndt_map_downsampler_file_path])
 
     # map downsample node execution definition
     map_downsampler_node_runner = launch_ros.actions.Node(
-        package="voxel_grid_nodes",
-        node_executable="voxel_grid_cloud_node_exe",
+        package='voxel_grid_nodes',
+        node_executable='voxel_grid_cloud_node_exe',
         parameters=[map_downsampler_param_file])
 
     return launch.LaunchDescription([
