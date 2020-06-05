99c54e628e29825d3aad212294a33a498e851570
MIURA Yasuyuki <kokosabu@gmail.com>
2020-06-05 20:50:36 +0900

    [#404] apply ament_auto macro to euclidean_cluster_nodes

--- src/perception/segmentation/euclidean_cluster_nodes/launch/euclidean_cluster_cloud_node.launch.py
+++ src/perception/segmentation/euclidean_cluster_nodes/launch/euclidean_cluster_cloud_node.launch.py
@@ -1,5 +1,4 @@
 # Copyright 2020 Apex.AI, Inc.
-# Co-developed by Tier IV, Inc. and Apex.AI, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Co-developed by Tier IV, Inc. and Apex.AI, Inc.
 
 """Launch euclidean cluster and ray ground classifier nodes."""
 
@@ -29,7 +30,7 @@ def generate_launch_description():
         get_package_share_directory('euclidean_cluster_nodes'),
         'param',
         'vlp16_lexus_cluster.param.yaml')
-    euclidean_cluster_param_file=launch.substitutions.LaunchConfiguration(
+    euclidean_cluster_param_file = launch.substitutions.LaunchConfiguration(
         'params', default=[vlp16_lexus_cluster_file_path])
 
     # ray ground classifier parameter file definition.
@@ -37,7 +38,7 @@ def generate_launch_description():
         get_package_share_directory('ray_ground_classifier_nodes'),
         'param',
         'vlp16_lexus.param.yaml')
-    ray_ground_param_file=launch.substitutions.LaunchConfiguration(
+    ray_ground_param_file = launch.substitutions.LaunchConfiguration(
         'params', default=[vlp16_lexus_ray_ground_file_path])
 
     # euclidean cluster node execution definition.
