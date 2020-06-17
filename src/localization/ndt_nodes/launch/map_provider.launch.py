9bc2b5e9b74dafa4b31a08fb6faa8dba06fda770
simon-t4 <simon.thompson@tier4.jp>
2020-06-17 23:27:26 +0000

    359 Resolve "Make YAML File/PCD Map Location a Required Parameter in Map Provider"

--- src/localization/ndt_nodes/launch/map_provider.launch.py
+++ src/localization/ndt_nodes/launch/map_provider.launch.py
@@ -18,26 +18,49 @@ import os
 
 from ament_index_python import get_package_share_directory
 import launch
-import launch.actions
-import launch.substitutions
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import launch_ros.actions
 
 
 def generate_launch_description():
 
+    map_data_prefix = get_package_share_directory('autoware_auto_avp_demo')
+
     # map_provider parameter file definition
     ndt_map_provider_file_path = os.path.join(
-        get_package_share_directory('ndt_nodes'),
-        'param',
-        'map_publisher.param.yaml')
-    map_provider_param_file = launch.substitutions.LaunchConfiguration(
-        'params', default=[ndt_map_provider_file_path])
+        map_data_prefix,
+        "param",
+        "map_publisher.param.yaml")
+    map_provider_param_file = LaunchConfiguration(
+        "params", default=[ndt_map_provider_file_path])
+
+    # map provide map file arguments
+    map_yaml_file_path = os.path.join(
+        map_data_prefix, "data/autonomoustuff_parking_lot_lgsvl.yaml"
+    )
+    map_pcd_file_path = os.path.join(
+        map_data_prefix, "data/autonomoustuff_parking_lot_lgsvl.pcd"
+    )
+    map_yaml_file_param = DeclareLaunchArgument(
+        'map_yaml_file_arg',
+        default_value=map_yaml_file_path,
+        description='Map YAML file describing map origin'
+    )
+    map_pcd_file_param = DeclareLaunchArgument(
+        'map_pcd_file_arg',
+        default_value=map_pcd_file_path,
+        description='Map PCD file containing 3D point cloud data'
+    )
 
     # map_provide node execution definition
     map_provider_node_runner = launch_ros.actions.Node(
-        package='ndt_nodes',
-        node_executable='ndt_map_publisher_exe',
-        parameters=[map_provider_param_file])
+        package="ndt_nodes",
+        node_executable="ndt_map_publisher_exe",
+        node_namespace="localization",
+        parameters=[map_provider_param_file,
+                    {"map_yaml_file": LaunchConfiguration('map_yaml_file_arg')},
+                    {"map_pcd_file": LaunchConfiguration('map_pcd_file_arg')}])
 
     # map downsampler paramter file definition
     ndt_map_downsampler_file_path = os.path.join(
@@ -57,6 +80,9 @@ def generate_launch_description():
             ("points_downsampled", "viz_ndt_map_downsampled")
         ])
 
+    # require a map file location
     return launch.LaunchDescription([
+        map_yaml_file_param,
+        map_pcd_file_param,
         map_provider_node_runner,
         map_downsampler_node_runner])
