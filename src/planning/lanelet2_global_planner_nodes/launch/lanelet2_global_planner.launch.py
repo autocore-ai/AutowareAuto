68cc8b700ea9d802200b108f31ff953036e5d228
Jilada Eccleston <jilada.eccleston@gmail.com>
2021-01-27 01:57:41 +0000

    Add basic node launch test for lanelet2_global_planner_nodes  - Add fixes to lanelet2_global_planner_launch file  - Remove reference to ament_lint_common

--- src/planning/lanelet2_global_planner_nodes/launch/lanelet2_global_planner.launch.py
+++ src/planning/lanelet2_global_planner_nodes/launch/lanelet2_global_planner.launch.py
@@ -18,7 +18,7 @@
 
 import launch.substitutions
 import launch_ros.actions
-from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     # execution definition.
