d3427d6a77cce81e63f5ba5a52a308792bcaabcd
Yunus Emre Caliskan <yunus.ec@gmail.com>
2021-05-11 15:34:50 +0200

    [1019] Change ndt optimization sanity test into a more isolated and analytical test.

--- src/localization/ndt/test/test_ndt_optimization.cpp
+++ src/localization/ndt/test/test_ndt_optimization.cpp
@@ -20,6 +20,8 @@
 #include <limits>
 #include "common/types.hpp"
 
+namespace
+{
 using autoware::common::types::bool8_t;
 using autoware::common::types::float32_t;
 using autoware::common::types::float64_t;
@@ -30,6 +32,25 @@ using autoware::localization::ndt::P2DNDTOptimizationConfig;
 using autoware::localization::ndt::transform_adapters::pose_to_transform;
 
 using P2DProblem = P2DNDTOptimizationProblem<autoware::localization::ndt::StaticNDTMap>;
+
+constexpr double kPoseEpsilon{0.01};
+
+sensor_msgs::msg::PointCloud2 create_xz_plane_point_cloud(
+  const PointXYZI & corner,
+  const float size,
+  const float step)
+{
+  sensor_msgs::msg::PointCloud2 msg;
+  point_cloud_msg_wrapper::PointCloud2Modifier<PointXYZI> msg_modifier{msg, "map"};
+  const auto intensity = 1.0F;
+  msg_modifier.push_back({corner.x, corner.y, corner.z, intensity});
+  for (auto coord = step; coord < size; coord += step) {
+    msg_modifier.push_back({corner.x + coord, corner.y, corner.z, intensity});
+    msg_modifier.push_back({corner.x, corner.y, corner.z + coord, intensity});
+  }
+  return msg;
+}
+}  // namespace
 
 OptTestParams::OptTestParams(
   float64_t x, float64_t y, float64_t z, float64_t ang_x, float64_t ang_y, float64_t ang_z,
@@ -64,7 +85,7 @@ protected:
   }
 };
 
-class P2DOptimizationValidationTest : public P2DOptimizationTest,
+class AlignmentXyzTest : public P2DOptimizationTest,
   public ::testing::WithParamInterface<OptTestParams> {};
 
 class P2DOptimizationNumericalTest : public P2DOptimizationTest,
@@ -101,12 +122,9 @@ TEST_P(P2DOptimizationNumericalTest, numerical_analysis) {
     }
   }
 }
-
-TEST_P(P2DOptimizationValidationTest, sanity_test) {
-  using PclCloud = pcl::PointCloud<pcl::PointXYZ>;
-  constexpr auto translation_tol = 1e-2;
-  constexpr auto rotation_tol = 1e-2;
-
+/// @test       The shape is fitting exactly into a single voxel. Its copy is moved in different
+///             directions and aligned with the original.
+TEST_P(AlignmentXyzTest, AlignShapesWithinOneVoxel) {
   const auto & param = GetParam();
   EigenPose<Real> diff = param.diff;
 
@@ -114,87 +132,91 @@ TEST_P(P2DOptimizationValidationTest, sanity_test) {
   diff_tf2.header.frame_id = "custom";
   pose_to_transform(diff, diff_tf2.transform);
 
-  // Ensure the scan won't be translated out of its voxel
-  ASSERT_LT(diff(0), m_voxel_size.x);
-  ASSERT_LT(diff(1), m_voxel_size.y);
-  ASSERT_LT(diff(2), m_voxel_size.z);
-  // translate the cloud by a small margin.
-  auto translated_cloud = m_downsampled_cloud;
-//  translate_pc(m_downsampled_cloud, translated_cloud, diff);
-  tf2::doTransform(m_downsampled_cloud, translated_cloud, diff_tf2);
-  ////////////// Solve using ndt optimization problem
+  // TODO(#1035): picking this smaller e.g. 0.31 breaks the test. We need to investigate why.
+  const auto shape_size = 0.4F;
+  const auto voxel_side_size = 0.5F;
+  const auto half_shape_size = 0.5F * shape_size;
+  const PointXYZI corner_coord{-half_shape_size, 0.0F, -half_shape_size, 0.0F};
+  ASSERT_LT(shape_size, voxel_side_size);
+  ASSERT_LT(corner_coord.x + shape_size + diff.x(), voxel_side_size) <<
+    "Moving the shape will move it out of the voxel which is not allowed in this test.";
+  ASSERT_LT(corner_coord.y + diff.y(), voxel_side_size) <<
+    "Moving the shape will move it out of the voxel which is not allowed in this test.";
+  ASSERT_LT(corner_coord.z + shape_size + diff.z(), voxel_side_size) <<
+    "Moving the shape will move it out of the voxel which is not allowed in this test.";
 
-  P2DProblem::DomainValue guess;
-  decltype(guess) pcl_guess;
-  guess.setZero();
-  pcl_guess.setZero();
-  const auto step_length = 0.12;
-  const auto num_iters = 5U;
-  {
-    P2DNDTScan scan(translated_cloud, translated_cloud.width);
-    P2DProblem problem{scan, m_static_map, P2DNDTOptimizationConfig{0.55}};
-    // Solve the problem in 50 iterations:
-    for (auto i = 0U; i < num_iters; ++i) {
-      problem.evaluate(
-        guess,
-        autoware::common::optimization::ComputeMode{}.set_score().set_jacobian().set_hessian());
-      P2DProblem::Jacobian jacobian;
-      P2DProblem::Hessian hessian;
-      problem.jacobian(guess, jacobian);
-      problem.hessian(guess, hessian);
-      // Solve for decent direction using newton method
-      Eigen::JacobiSVD<Eigen::Matrix<float64_t, 6, 6>> sv(hessian,
-        Eigen::ComputeFullU | Eigen::ComputeFullV);
-      // Negative for maximization as opposed to minimization
-      decltype(guess) delta = sv.solve(-jacobian);
-      guess += step_length * delta;
-    }
+  // Pick a bigger grid but offset it by half the voxel size to make sure 0,0,0 is in the center of
+  // a voxel in the middle of the grid.
+  const auto half_grid_extent = 2.0F + 0.5F * voxel_side_size;
+  geometry_msgs::msg::Point32 min_point;
+  min_point.set__x(-half_grid_extent).set__y(-half_grid_extent).set__z(-half_grid_extent);
+  geometry_msgs::msg::Point32 max_point;
+  max_point.set__x(half_grid_extent).set__y(half_grid_extent).set__z(half_grid_extent);
+  geometry_msgs::msg::Point32 voxel_size;
+  voxel_size.set__x(voxel_side_size).set__y(voxel_side_size).set__z(voxel_side_size);
+  const auto capacity = 1000U;
+  autoware::perception::filters::voxel_grid::Config grid_config{
+    min_point,
+    max_point,
+    voxel_size,
+    capacity};
+  // Create a plane-like point cloud that fits within a single voxel of the grid. The coordinates
+  // are picked so that this plane lies in the center of the center-most voxel of the grid.
+  const auto step = 0.01F;
+  const auto initial_point_cloud = create_xz_plane_point_cloud(corner_coord, shape_size, step);
+  ASSERT_GT(initial_point_cloud.width, 1U);
+  autoware::localization::ndt::DynamicNDTMap dynamic_map{grid_config};
+  dynamic_map.insert(initial_point_cloud);
+  ASSERT_EQ(dynamic_map.size(), 1U);
+
+  autoware::localization::ndt::StaticNDTMap static_map{};
+  sensor_msgs::msg::PointCloud2 serialized_dynamic_map;
+  dynamic_map.serialize_as<decltype(static_map)>(serialized_dynamic_map);
+  static_map.set(serialized_dynamic_map);
+  ASSERT_EQ(static_map.size(), 1U);
+
+  auto translated_cloud = initial_point_cloud;
+  tf2::doTransform(initial_point_cloud, translated_cloud, diff_tf2);
+
+  P2DProblem::DomainValue guess = P2DProblem::DomainValue::Zero();
+  const auto step_length = 0.1F;
+  const auto num_iters = 100U;
+  P2DNDTScan scan{translated_cloud, translated_cloud.width};
+  // TODO(#1062): we have no outliers here, but setting this to 0.0 fails the test. Investigate.
+  const auto dummy_outlier_ratio = 0.01F;
+  P2DProblem problem{scan, static_map, P2DNDTOptimizationConfig{dummy_outlier_ratio}};
+
+  for (auto i = 0U; i < num_iters; ++i) {
+    problem.evaluate(
+      guess,
+      autoware::common::optimization::ComputeMode{}.set_score().set_jacobian()
+      .set_hessian());
+    P2DProblem::Jacobian jacobian;
+    P2DProblem::Hessian hessian;
+    problem.jacobian(guess, jacobian);
+    problem.hessian(guess, hessian);
+    P2DProblem::DomainValue delta = hessian.ldlt().solve(-jacobian);
+    guess += step_length * delta;
+    auto guess_cloud = translated_cloud;
+    pose_to_transform(guess, diff_tf2.transform);
+    tf2::doTransform(translated_cloud, guess_cloud, diff_tf2);
   }
 
-  decltype(diff) neg_diff = -diff;
-  is_pose_approx(guess, neg_diff, translation_tol, rotation_tol);
-
-  std::cout << "The scan is transformed away from the map by: \n" << diff << std::endl;
-  std::cout << "The estimated pose difference after " << num_iters << " iterations with " <<
-    step_length << " step size: \n" << guess << std::endl;
-
-  // Compare to the pcl result
-  if (param.check_pcl) {
-    PclCloud::Ptr scan_cloud{new PclCloud};
-    PclCloud::Ptr map_cloud{new PclCloud};
-    PclCloud::Ptr result_cloud{new PclCloud};
-    *scan_cloud = from_pointcloud2(translated_cloud);
-    *map_cloud = from_pointcloud2(m_pc);
-
-    pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> pcl_ndt;
-    Eigen::Transform<float32_t, 3, Eigen::Affine, Eigen::ColMajor> init_guess;
-    init_guess.matrix().setIdentity();
-    pcl_ndt.setInputSource(scan_cloud);
-    pcl_ndt.setInputTarget(map_cloud);
-    pcl_ndt.align(*result_cloud, init_guess.matrix());
-
-    init_guess.matrix() = pcl_ndt.getFinalTransformation();
-    pcl_guess.head(3) = init_guess.translation().cast<float64_t>();
-    pcl_guess.tail(3) = init_guess.rotation().eulerAngles(0, 1, 2).cast<float64_t>();
-
-    // Using high error tolerance since two different implementations may vary in result greatly.
-    is_pose_approx(pcl_guess, neg_diff, 0.2, 0.1);
-
-    std::cout << "PCL implementation's estimated pose difference: \n" << pcl_guess << std::endl;
+  const auto found_diff = P2DProblem::DomainValue{-guess};
+  for (int i = 0; i < found_diff.size(); ++i) {
+    EXPECT_NEAR(found_diff[i], diff[i], kPoseEpsilon) << "Not matching at index " << i;
   }
 }
 
-// TODO(yunus.caliskan): Make the sample scan more 'featureful' so that ndt can match
-// and converge for larger drifts as well with a higher accuracy.
+
+// TODO(#1063): when these parameters are picked bigger the tests fail. We need to investigate why.
 INSTANTIATE_TEST_CASE_P(
-  sanity_test, P2DOptimizationValidationTest,
+  AlignmentXyzTest, AlignmentXyzTest,
   ::testing::Values(
-    // TODO(yunus.caliskan): Make voxel grid configuration similar to that of PCL
-    // to get results that are more comparable.
-    OptTestParams{0.0, 0.65, 0.0, 0.0, 0.0, 0.0, true, false},
-    OptTestParams{0.7, 0.0, 0.7, 0.0, 0.0, 0.0, true, true},
-    OptTestParams{0.0, 0.1, 0.1, 0.0, 3.14159265359 / 72.0, 0.0, false, false},
-    OptTestParams{0.0, -0.2, 0.0, 0.0, 3.14159265359 / 72.0, 3.14159265359 / 72.0, false, false}
+    OptTestParams{0.2, 0.0, 0.0, 0.0, 0.0, 0.0, false, false},
+    OptTestParams{0.0, 0.1, 0.0, 0.0, 0.0, 0.0, false, false},
+    OptTestParams{0.0, 0.0, 0.2, 0.0, 0.0, 0.0, false, false},
+    OptTestParams{0.1, 0.05, 0.1, 0.0, 0.0, 0.0, false, false}
     // cppcheck-suppress syntaxError
   ), );
 
